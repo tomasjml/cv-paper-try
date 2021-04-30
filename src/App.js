12864a539b91f2e4c31462acdf6b746d1852794c
Tomas Molina <jtml.mass@gmail.com>
2021-04-29 23:46:18 -0400

    Pick up font and Start my information

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { useEffect } from "react";
 
 export default function App() {
   useEffect(() => {
-    document.title = "CV Paper Try";
+    document.title = "Tomas Molina";
   }, []);
 
   return (
     <div className="App">
       <div className="paper">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <h1>Tomas Molina</h1>
+        <h2>System and Software Engineer</h2>
       </div>
     </div>
   );
